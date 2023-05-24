a4f58d47b4fc3df5541f61fd3899e06c448f8db9
Messaoud-SETTOU <messoudsettou@gmail.com>
2023-05-24 11:19:27 +0100

    "add vue components"

--- vue frontend/src/App.vue
+++ vue frontend/src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import { defineComponent } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import Hello from './components/Hello.vue'
+import Home from './components/Home.vue'
+import Login from './components/Login.vue'
+import SignUp from './components/SignUp.vue'
+import Products from './components/Products.vue'
+import Compte from './components/Compte.vue'
+import Help from './components/Help.vue'
+
 </script>
 
 <template>
-     <Hello ></Hello>
-   
+  <Home />
+  
+ 
 </template>
-
-
--- vue frontend/src/router/index.ts
+++ vue frontend/src/router/index.ts
@@ -1,15 +1,59 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Hello from '../components/Hello.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../components/Home.vue';
+import Login from '../components/Login.vue';
+import SignUp from '../components/SignUp.vue';
+import Products from '../components/Products.vue';
+import Compte from '../components/Compte.vue';
+import Help from '../components/Help.vue';
+import SideBar from '../components/SideBar.vue';
+import Navbar from '../components/NavBar.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/SignUp',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/Products',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/Compte',
+    name: 'Compte',
+    component: Compte
+  },
+  {
+    path: '/Help',
+    name: 'Help',
+    component: Help
+  },
+  {
+    path: '/SideBar',
+    name: 'SideBar',
+    component: SideBar
+  },
+  {
+    path: '/NavBar',
+    name: 'NavBar',
+    component: Navbar
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Hello
-    }
-  ]
-})
+  routes
+});
 
-export default router
+export default router;

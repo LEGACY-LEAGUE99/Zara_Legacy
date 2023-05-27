ab6f8e56ff34da3299e00755e968b704d7409be4
ahmedcheraif31 <ahmedcheraif31@gmail.com>
2023-05-27 15:47:51 +0100

    done

--- backend/controllers/HelpController.ts
+++ backend/controllers/HelpController.ts
@@ -3,20 +3,28 @@ import { Request, Response } from 'express';
 import Help from '../models/HelpModel';
 
  const Helps = async (req: Request, res: Response) => {
-
-  
   try {
-    const helpPage = await Help.findOne({ titel: req.params.titel });
-  
+    const helpPage = await Help.findOne({ topic: req.params.topic });
     if (!helpPage) {
       return res.status(404).json({ message: 'Help page not found.' });
     }
-  
     return res.json(helpPage);
   } catch (error) {
     return res.status(500).json({ message: 'Internal server error.' });
   }
 };
 
+const Segg = async (req: Request, res: Response) => {
+  try {
+    const searchQuery = req.query.query;
+    // Perform a MongoDB query to retrieve suggestions based on the searchQuery
+    const suggestions = await Help.find({ topic: { $regex: searchQuery, $options: 'i' } }).limit(10);
+    return res.json(suggestions);
+  } catch (error) {
+    console.log(error);
+    
+    return res.status(500).json({ message: 'Internal server error.' });
+  }
+};
 
-export default Helps
+export default { Helps, Segg }
--- vuefrontend/src/components/Help.vue
+++ vuefrontend/src/components/Help.vue
@@ -1,67 +1,86 @@
 <template>
-<div>
-    <SideBar/>
-    <input type="text" v-model="searchTitle" placeholder="Search" />
-    <button @click="searchHelp">Search</button>
-
-    <div v-if="isLoading">Loading...</div>
-    <div v-else-if="errorMessage">{{ errorMessage }}</div>
-    <div v-else-if="helpPage">
-      <h2>FAQ: {{ helpPage.titel }}</h2>
-      <p>Q: {{ helpPage.question }}</p>
-      <p>A: {{ helpPage.answer }}</p>
+    <div>
+      <SideBar/>
+      <input type="text" v-model="searchTopic" placeholder="Search" @input="fetchSuggestions" />
+      <ul v-if="suggestions.length">
+        <li v-for="(suggestion, index) in suggestions" :key="index">{{ suggestion.title }}</li>
+      </ul>
+  
+      <button @click="searchHelp">Search</button> <!-- Added button element -->
+  
+      <div v-if="isLoading">Loading...</div>
+      <div v-else-if="errorMessage">{{ errorMessage }}</div>
+      <div v-else-if="helpPage">
+        <h1>Topic: {{ helpPage.topic }} </h1>
+        <h2>FAQ: {{ helpPage.title }}</h2>
+        <p>Question: {{ helpPage.question }}</p>
+        <p>Answer: {{ helpPage.answer }}</p>
+      </div>
     </div>
-  </div>
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import axios from 'axios';
-import SideBar from './SideBar.vue';
-
-interface HelpPage {
-  title: string;
-  question: string;
-  Answer: string;
-}
-
-export default defineComponent({
+  </template>
+  
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import axios from 'axios';
+  import SideBar from './SideBar.vue';
+  
+  interface HelpPage {
+    title: string;
+    question: string;
+    answer: string;
+  }
+  
+  export default defineComponent({
     data() {
-        return {
-            searchTitle: "",
-            isLoading: false,
-            errorMessage: "",
-            helpPage: null as HelpPage | any,
-        };
+      return {
+        searchTopic: "",
+        isLoading: false,
+        errorMessage: "",
+        helpPage: null as HelpPage | any,
+        suggestions: [] as HelpPage[],
+      };
     },
     methods: {
-        async searchHelp() {
-            try {
-                this.isLoading = true;
-                this.errorMessage = "";
-                this.helpPage = null;
-                const response = await axios.get(`http://localhost:3000/help/${this.searchTitle}`);
-                this.helpPage = response.data;
-                console.log(this.helpPage.answer);
-            }
-            catch (error: any) {
-                if (error.response && error.response.status === 404) {
-                    this.errorMessage = "Help page not found.";
-                }
-                else {
-                    this.errorMessage = "Internal server error.";
-                }
-            }
-            finally {
-                this.isLoading = false;
-            }
-        },
+      async fetchSuggestions() {
+        try {
+          if (this.searchTopic.length >= 3) {
+            this.isLoading = true;
+            const response = await axios.get(`http://localhost:3000/segg`, {
+              params: {
+                query: this.searchTopic,
+              },
+            });
+            this.suggestions = response.data;
+          } else {
+            this.suggestions = [];
+          }
+        } catch (error) {
+          console.error(error);
+        } finally {
+          this.isLoading = false;
+        }
+      },
+      async searchHelp() {
+        try {
+          this.isLoading = true;
+          this.errorMessage = "";
+          this.helpPage = null;
+          const response = await axios.get(`http://localhost:3000/help/${this.searchTopic}`);
+          this.helpPage = response.data;
+          console.log(response.data);
+        } catch (error: any) {
+          if (error.response && error.response.status === 404) {
+            this.errorMessage = "Help page not found.";
+          } else {
+            this.errorMessage = "Internal server error.";
+          }
+        } finally {
+          this.isLoading = false;
+        }
+      },
     },
-    components: { SideBar }
-});
-</script>
-
-<style scoped>
-
-
-</style>
+    components: { SideBar },
+  });
+  </script>
+  
